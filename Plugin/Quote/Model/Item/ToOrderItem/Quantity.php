b1d977153ee750e9bb6e1bafe95b68c092082593
Aurélien Lavorel <aurelien@lavoweb.net>
2018-11-18 12:57:32 +0100

    fix qty in order

---
+++ Plugin/Quote/Model/Item/ToOrderItem/Quantity.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace C4B\FreeProduct\Plugin\Quote\Model\Item\ToOrderItem;
+
+use C4B\FreeProduct\SalesRule\Action\GiftAction;
+use \Magento\Quote\Model\Quote\Item\ToOrderItem as Source;
+
+/**
+ * @category   C4B
+ * @package    C4B_FreeProduct
+ * @author     Aurélien Lavorel <aurelien@lavoweb.net>
+ * @copyright  code4business Software GmbH
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ */
+class Quantity
+{
+    /**
+     * Force qty for gift items
+     *
+     * @plugin after
+     * @param Source $subject
+     * @param $result
+     * @return $result
+     */
+    public function afterConvert(Source $subject, $result)
+    {
+        if ($result->getData('product_type') == GiftAction::PRODUCT_TYPE_FREEPRODUCT) {
+            $result->setQtyOrdered(1);
+        }
+        return $result;
+    }
+}
